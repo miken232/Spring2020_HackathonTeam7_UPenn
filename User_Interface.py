3618517bc29baf8060f06c599e685566b75f456c
mnarcisi <mnarcisi@>
2020-01-09 23:23:47 -0500

    Add simple title page - not finished

---
+++ User_Interface.py
@@ -0,0 +1,50 @@
+from tkinter import *
+
+# Create classes to represent the Welcome Page, User Input Page, and Results Page
+class WelcomePage:
+    def __init__(self, master):
+        # Prevent user from changing the window size
+        master.geometry("750x750")  # Gives a starting geometry to the window
+        master.resizable(width=False, height=False)
+
+        # Initialize Frames
+        #title_frame = Frame(master)
+        #title_frame.pack()
+        #text_frame = Frame(master)
+        #text_frame.pack(side=BOTTOM)
+
+        # Create welcome screen text labels
+        self.title_label = Label(master, text="New York City Property Value Predictor", bg="darkblue", fg="white")
+        self.title_label.config(height=5, width=40)
+        self.title_label.config(font=("Courier", 30))
+        self.title_label.pack()
+        self.text_label = Label(master, text="Please enter the following property information to receiving a "
+                                             "purchase price prediction.", bg="lightgray", wraplength=725)
+        self.text_label.config(height=5, width=60)
+        self.text_label.config(font=("Courier", 20))
+        self.text_label.pack()
+
+
+        # Create buttons to continue the application
+        #self.continue_button = Button(master, text="Continue")
+        #self.continue_button.grid(sticky=W)
+        #self.quit_button = Button(master, text="Quit")
+        #self.quit_button.grid(row=0, column=10, sticky=W)
+
+class UserInputPage:
+    def __init__(self, master):
+        pass
+
+
+class ResultsPage:
+    def __init__(self, master):
+        pass
+
+
+# Create the base window
+base = Tk()
+
+# Create
+welcome = WelcomePage(base)
+
+base.mainloop()
